970d7d4fa11ee1e1e2c659a7633663afee26fa70
ilyakozhevn <kozhevilya@gmail.com>
2022-07-27 13:16:28 +0300

    WIP: 	Reference storyboard added 	getFunds

---
+++ HomeWorkFor2.13Lesson(Wallet)/Services/DataStore.swift
@@ -0,0 +1,37 @@
+//
+//  DataStore.swift
+//  HomeWorkFor2.13Lesson(Wallet)
+//
+//  Created by Ilya Kozhevnikov on 27.07.2022.
+//
+
+
+//let typeOfFunds: TypeOfFunds
+//let image: String
+//let name: String
+//let currency: Currency
+//var price: Double
+//var quantity: Int
+
+class DataStore {
+    let fundsType: TypeOfFunds = .stock
+    
+    let images = [
+        "phone",
+        "tray"
+    ]
+    
+    let names = [
+        "Apple",
+        "Google",
+        "Amazon",
+        "Netflix",
+        "Whatever",
+        "Newbie",
+        "OKOK",
+        "Lays"
+    ]
+    
+    let currency: Currency = .usd
+    
+}
